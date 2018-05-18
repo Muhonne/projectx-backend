fc3d6841d5399b1f012e3cc87a3ef1c02bf6b05a
Mattias Muhonen <mattias.muhonen@soikea.com>
2018-05-18 11:55:52 +0300

    get to grips with heroku config vars

--- src/environment.js
+++ src/environment.js
@@ -1,19 +1,5 @@
-const axios = require("axios");
-
 module.exports = {
   secret: "salainen jwt sala juttu",
   localMongo: "mongodb://localhost/projectx-dev",
-  productionMongo: () => {
-    axios
-      .get("https://api.heroku.com/apps/projectx-backend/config-vars", {
-        headers: {
-          Accept: " application/vnd.heroku+json; version=3"
-        }
-      })
-      .then(response => console.log("response", response))
-      .catch(err => {
-        console.log("error", err);
-        console.log("proces env", process.env);
-      });
-  }
+  productionMongo: process.env.MONGO || 'missing from environment'
 };
